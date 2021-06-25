0af3b013cb803417d3452e55fb7431aa0121988c
mikebarnes-farmiq <85854565+mikebarnes-farmiq@users.noreply.github.com>
2021-06-25 15:28:55 +1200

    Update networks.tf

--- terraform/networks.tf
+++ terraform/networks.tf
@@ -2,55 +2,55 @@ data "aws_vpc" "farmiq_test" {
   id = var.vpc_id
 }
 
-resource "aws_security_group" "allow_all_in_vpc" {
-  name   = "allow-all-in-vpc-${terraform.workspace}"
-  vpc_id = data.aws_vpc.farmiq_test.id
-  ingress {
-    description = "allow all in bound request"
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = [data.aws_vpc.farmiq_test.cidr_block]
-  }
+# resource "aws_security_group" "allow_all_in_vpc" {
+#   name   = "allow-all-in-vpc-${terraform.workspace}"
+#   vpc_id = data.aws_vpc.farmiq_test.id
+#   ingress {
+#     description = "allow all in bound request"
+#     from_port   = 0
+#     to_port     = 65535
+#     protocol    = "tcp"
+#     cidr_blocks = [data.aws_vpc.farmiq_test.cidr_block]
+#   }
 
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-}
+#   egress {
+#     from_port        = 0
+#     to_port          = 0
+#     protocol         = "-1"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#     ipv6_cidr_blocks = ["::/0"]
+#   }
+# }
 
-resource "aws_security_group" "allow_http_https" {
-  name   = "allow-http-https-${terraform.workspace}"
-  vpc_id = data.aws_vpc.farmiq_test.id
-  ingress {
-    description      = "Allow https access"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
+# resource "aws_security_group" "allow_http_https" {
+#   name   = "allow-http-https-${terraform.workspace}"
+#   vpc_id = data.aws_vpc.farmiq_test.id
+#   ingress {
+#     description      = "Allow https access"
+#     from_port        = 443
+#     to_port          = 443
+#     protocol         = "tcp"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#     ipv6_cidr_blocks = ["::/0"]
+#   }
 
-  ingress {
-    description      = "Allow http access"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
+#   ingress {
+#     description      = "Allow http access"
+#     from_port        = 80
+#     to_port          = 80
+#     protocol         = "tcp"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#     ipv6_cidr_blocks = ["::/0"]
+#   }
 
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-}
+#   egress {
+#     from_port        = 0
+#     to_port          = 0
+#     protocol         = "-1"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#     ipv6_cidr_blocks = ["::/0"]
+#   }
+# }
 
 resource "aws_lb" "farmiq" {
   name = "farmiq-lb-${terraform.workspace}"
