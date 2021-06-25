022e0ebd9b2d3b18df89add7706ada3df7440d2a
mikebarnes-farmiq <85854565+mikebarnes-farmiq@users.noreply.github.com>
2021-06-25 15:34:30 +1200

    Update networks.tf

--- terraform/networks.tf
+++ terraform/networks.tf
@@ -22,42 +22,42 @@ data "aws_vpc" "farmiq_test" {
 #   }
 # }
 
-# resource "aws_security_group" "allow_http_https" {
-#   name   = "allow-http-https-${terraform.workspace}"
-#   vpc_id = data.aws_vpc.farmiq_test.id
-#   ingress {
-#     description      = "Allow https access"
-#     from_port        = 443
-#     to_port          = 443
-#     protocol         = "tcp"
-#     cidr_blocks      = ["0.0.0.0/0"]
-#     ipv6_cidr_blocks = ["::/0"]
-#   }
+resource "aws_security_group" "allow_http_https_lb" {
+  name   = "allow-http-https-${terraform.workspace}"
+  vpc_id = data.aws_vpc.farmiq_test.id
+  ingress {
+    description      = "Allow https access"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
-#   ingress {
-#     description      = "Allow http access"
-#     from_port        = 80
-#     to_port          = 80
-#     protocol         = "tcp"
-#     cidr_blocks      = ["0.0.0.0/0"]
-#     ipv6_cidr_blocks = ["::/0"]
-#   }
+  ingress {
+    description      = "Allow http access"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
-#   egress {
-#     from_port        = 0
-#     to_port          = 0
-#     protocol         = "-1"
-#     cidr_blocks      = ["0.0.0.0/0"]
-#     ipv6_cidr_blocks = ["::/0"]
-#   }
-# }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
 
 resource "aws_lb" "farmiq" {
   name = "farmiq-lb-${terraform.workspace}"
 
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.allow_http_https.id]
+  security_groups    = [aws_security_group.allow_http_https_lb.id]
   subnets            = [var.subnet_a, var.subnet_b]
   enable_deletion_protection = false
 }
